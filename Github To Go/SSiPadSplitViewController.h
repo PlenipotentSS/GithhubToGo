9fc211de6f5287a035a6a71147f75f9b7ba3a801
PlenipotentSS <stevensonhoyt@icloud.com>
2014-01-29 00:02:31 -0800

    Finishing Touches

---
+++ Github To Go/SSiPadSplitViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSiPadSplitViewController.h
+//  Github To Go
+//
+//  Created by Stevenson on 1/28/14.
+//  Copyright (c) 2014 Steven Stevenson. All rights reserved.
+//
+
+#import "SSSplitViewController.h"
+
+@interface SSiPadSplitViewController : UISplitViewController
+
+@end
---
+++ Github To Go/SSiPadSplitViewController.m
@@ -0,0 +1,38 @@
+//
+//  SSiPadSplitViewController.m
+//  Github To Go
+//
+//  Created by Stevenson on 1/28/14.
+//  Copyright (c) 2014 Steven Stevenson. All rights reserved.
+//
+
+#import "SSiPadSplitViewController.h"
+
+@interface SSiPadSplitViewController ()
+
+@end
+
+@implementation SSiPadSplitViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
